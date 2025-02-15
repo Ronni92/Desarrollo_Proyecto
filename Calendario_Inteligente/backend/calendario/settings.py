919d645497632772281d389a39779299f1f65c77
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-02-15 16:18:49 -0500

    Conexiones correctas a la base de datos

--- Calendario_Inteligente/backend/calendario/settings.py
+++ Calendario_Inteligente/backend/calendario/settings.py
@@ -22,7 +22,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'calendario_db',
         'USER': 'postgres',
-        'PASSWORD': 'tu_password',
+        'PASSWORD': 'Ares:1997',
         'HOST': 'localhost',
         'PORT': '5432',
     }
